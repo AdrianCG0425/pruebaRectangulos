f4bcc1850196890ca52791f03787c1668cbe6bda
AdrianCuervoG <adry0425@hotmail.com>
2020-10-09 12:47:49 -0500

    inicio

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x,y,a,h;
+    cout << "ingrese las coordenadas del orige en x" << endl;
+    cin>>x;
+    cout << "ingrese las coordenadas del orige en y" << endl;
+    cin>>y;
+    cout << "ingrese la medida del ancho del rectangulo" << endl;
+    cin>>a;
+    cout << "ingrese la medida del alto del rectangulo" << endl;
+    cin>>h;
+    return 0;
+}
